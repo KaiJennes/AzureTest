5fd664e4e655ea9deb11170d7f2ede73ca604add
Kai-Philip Jennes <Kai-Philip.Jennes@maass.it>
2023-05-10 13:31:03 +0200

    init commit

---
+++ Shared/Enum/TicketHeaderState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTest.Shared.Enum
+{
+    public enum TicketHeaderState
+    {
+        Delegate = 0,
+        InProgress = 1,
+        WaitingForCustomer=2,
+        Finished = 3
+    }
+}
