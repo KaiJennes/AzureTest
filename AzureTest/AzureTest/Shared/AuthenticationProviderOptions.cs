50dd9dde4818ad9b64cfe55c24d49a238e2497ad
Kai-Philip Jennes <Kai-Philip.Jennes@maass.it>
2023-05-19 09:05:01 +0200

    changed to wasm standalone

--- AzureTest/AzureTest/Shared/AuthenticationProviderOptions.cs
+++ AzureTest/AzureTest/Shared/AuthenticationProviderOptions.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AzureTest.Shared
 {
-    public static class AuthenticationProvider
+    public static class AuthenticationProviderOptions
     {
         public static string AzureAd = ".auth/login/aad";
     }
